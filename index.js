4d7d551847b3cf823adc39f784cd89f8e393620d
Michel Tomas <michel@kissmy.co>
2020-04-18 22:16:27 +0200

    add minimal error handling

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ const port = process.env.APP_PORT || 3000
 
 app.use(cors())
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 app.use(bodyParser.json())
 
 app.post('/relay', async (req, res) => {
+  if (!req.query.urls) {
+    res.status(400).send('No `urls[]` param found, please check your configuration.')
+  }
+  if (!req.body.payload) {
+    res.status(400).send('Could not parse `body.payload`. Check you\'ve setup your integration correctly.')
+  }
   const urls = req.query.urls.map(url => decodeURI(url))
 
-  await Promise.all(urls.map(url => axios.post(url, req.body)))
-  res.end('Requests transfered!')
+  
+  await Promise.all(urls.map(url => axios.post(url, req.body.payload)))
+    .catch((e) => {
+      console.log(e)
+      console.log(e.response)
+      res.status(500).send('Failed to transfer request (see errors above)!')
+  })
+  res.status(200).send('Requests transfered!')
 })
 
 app.listen(port, () => {
